555d85dfe3a893c270b290cc890016ff51a7d9e0
AryaDeb1234 <aryadeb2005@gmail.com>
2025-07-11 13:58:25 +0530

    updata db

--- models/task.js
+++ models/task.js
@@ -1,12 +1,28 @@
+// const mongoose = require("mongoose");
+
+// const taskSchema = new mongoose.Schema({
+//   title: { type: String, required: true },
+//   description: String,
+//   completed: { type: Boolean, default: false },
+//   user: { type: mongoose.Schema.Types.ObjectId, ref: "user" }, // Reference to User
+// }, 
+// { timestamps: true }
+// );
+
+// module.exports = mongoose.model("task", taskSchema);
+
 const mongoose = require("mongoose");
 
-const taskSchema = new mongoose.Schema({
-  title: { type: String, required: true },
-  description: String,
-  completed: { type: Boolean, default: false },
-  user: { type: mongoose.Schema.Types.ObjectId, ref: "user" }, // Reference to User
-}, 
-{ timestamps: true }
+const taskSchema = new mongoose.Schema(
+  {
+    title: { type: String, required: true },
+    description: String,
+    completed: { type: Boolean, default: false },
+    user: { type: mongoose.Schema.Types.ObjectId, ref: "user" }, // Reference to User
+    createdAt: { type: Date, default: Date.now },  // Set default to current time
+    updatedAt: { type: Date, default: Date.now },  // Set default to current time
+  },
+  { timestamps: true }
 );
 
 module.exports = mongoose.model("task", taskSchema);
