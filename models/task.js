da3d2aa2029ca564960a0f31d54da1ed48d92749
Arya Deb <aryadeb2005@gmail.com>
2025-12-31 13:26:22 +0530

    update task

--- models/task.js
+++ models/task.js
@@ -1,15 +1,3 @@
-// const mongoose = require("mongoose");
-
-// const taskSchema = new mongoose.Schema({
-//   title: { type: String, required: true },
-//   description: String,
-//   completed: { type: Boolean, default: false },
-//   user: { type: mongoose.Schema.Types.ObjectId, ref: "user" }, // Reference to User
-// }, 
-// { timestamps: true }
-// );
-
-// module.exports = mongoose.model("task", taskSchema);
 
 const mongoose = require("mongoose");
 
